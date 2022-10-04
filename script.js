b9c4e3c7399a8bbc38957de363d256fde7ec25e3
Luis Sanchez <luisanchezdh@gmail.com>
2022-10-04 13:33:59 -0400

    HTML/CSS/JS file with class of questions added

---
+++ script.js
@@ -0,0 +1,104 @@
+/* grid with question on top and 4 answers, there should be a next/submit button and it should throw your score at the end ) */
+
+
+let questions = [
+  {
+    question: "Illmatic 1994",
+    a: "Biggie",
+    b: "Jay Z",
+    c: "Drake",
+    d: "Nas",
+    answer: "D"
+  },
+
+  {
+    question: "My Beautiful Dark Twisted Fantasy",
+    a: "Drake",
+    b: "Frank Ocean",
+    c: "Andre 3000",
+    d: "Kanye West",
+    answer: "d"
+  },
+
+  {
+    question: "Get Rich or Die Tryin",
+    a: "50 cent",
+    b: "Jadakiss",
+    c: "DMX",
+    d: "Lloyd Banks",
+    answer: "a",
+  },
+
+  {
+    question: "Come Home with Me",
+    a: "Juelz Santana",
+    b: "Lil wayne",
+    c: "Camron",
+    d: "Eminem",
+    answer: "c"
+  },
+
+  {
+    question: "",
+    a: "",
+    b: "",
+    c: "",
+    d: "" ,
+    answer: ""
+    
+  },
+
+  {
+    question: "",
+    a: "",
+    b: "",
+    c: "",
+    d: "" ,
+    answer: ""
+    
+  },
+
+  {
+    question: "",
+    a: "",
+    b: "",
+    c: "",
+    d: "" ,
+    answer: ""
+    
+  },
+
+  {
+    question: "",
+    a: "",
+    b: "",
+    c: "",
+    d: "" ,
+    answer: ""
+    
+  },
+
+  {
+    question: "",
+    a: "",
+    b: "",
+    c: "",
+    d: "" ,
+    answer: ""
+    
+  },
+
+  {
+    question: "",
+    a: "",
+    b: "",
+    c: "",
+    d: "" ,
+    answer: ""
+    
+  }
+
+
+]
+
+
